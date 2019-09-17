ef296bfba28632b2f1c0cdcee3c11395166c6d2d
liupeng <37412816+liupengzhouyi@users.noreply.github.com>
2019-09-17 19:27:17 +0800

    测试项目运行.成功 V 1.0.3

---
+++ src/main/java/cn/liupeng/catering_management_system/controller/PalyController.java
@@ -0,0 +1,25 @@
+package cn.liupeng.catering_management_system.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class PalyController {
+
+
+
+    @RequestMapping("say")
+    public String sayHelloWorld() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("index")
+    public String gotoIndex() {
+        return "index";
+    }
+
+
+
+}
